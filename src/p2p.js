c818a21f84027cd7b78c2b4f8c7790b93777e4c7
seunghun <seunghun.chan@gmail.com>
2019-02-21 00:06:32 +0900

    Send the message each peers

--- src/p2p.js
+++ src/p2p.js
@@ -8,14 +8,18 @@ const startP2PServer = server => {
   const wsServer = new WebSockets.Server({ server });
 
   wsServer.on('connection', ws => {
-    console.log(`Hello Socket!`);
+    initSocketConnection(ws);
   });
-
   console.log(`Nomadcoin P2PServer Runnging!`);
 };
 
 const initSocketConnection = socket => {
   sockets.push(socket);
+  socket.on('message', data => {
+    console.log(data);
+  });
+
+  setTimeout(() => socket.send('Welcome'), 5000);
 };
 
 const connectToPeers = newPeer => {
