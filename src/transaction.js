91260ac6e61b5be5fa9d601e95c6e987a0337109
huna <huna@wemakeprice.com>
2019-03-10 00:42:01 +0900

    Added validation of structure of transactions

--- src/transaction.js
+++ src/transaction.js
@@ -47,7 +47,9 @@ const getTxId = tx => {
 };
 
 const findUTxOut = (txOutId, txOutIndex, uTxOutsList) => {
-  return uTxOutsList.find(uTxOut => uTxOut.txOutId === txOutId && uTxOut.txOutIndex === txOutIndex);
+  return uTxOutsList.find(
+    uTxOut => uTxOut.txOutId === txOutId && uTxOut.txOutIndex === txOutIndex
+  );
 };
 
 const signTxIn = (tx, txInIndex, privateKey, uTxOut) => {
@@ -64,18 +66,82 @@ const signTxIn = (tx, txInIndex, privateKey, uTxOut) => {
 
 const updateUTxOuts = (newTxs, uTxOutList) => {
   const newUTxOuts = newTxs
-    .map(tx => tx.txOuts
-      .map((txOut, index) => new UTxOut(tx.id, index, txOut.address, txOut.amount)))
+    .map(tx =>
+      tx.txOuts.map(
+        (txOut, index) => new UTxOut(tx.id, index, txOut.address, txOut.amount)
+      )
+    )
     .reduce((a, b) => a.concat(b), []);
 
   const spentTxOuts = newTxs
     .map(tx => tx.txIns)
     .reduce((a, b) => a.concat(b), [])
-    .map(txIn => new UTxOut(txIn.txOutId, txIn.txOutIndex, '', 0));
+    .map(txIn => new UTxOut(txIn.txOutId, txIn.txOutIndex, "", 0));
 
   const resultingUTxOuts = uTxOutList
     .filter(uTxO => !findUTxOut(uTxO.txOutId, uTxO.txOutIndex, spentTxOuts))
     .concat(newUTxOuts);
 
   return resultingUTxOuts;
+};
+
+const isTxInStructureValid = txIn => {
+  if (typeof txIn === null) {
+    return false;
+  } else if (typeof txIn.signature !== "string") {
+    return false;
+  } else if (typeof txIn.txOutId !== "string") {
+    return false;
+  } else if (typeof txIn.txOutIndex !== "number") {
+    return false;
+  } else {
+    return true;
+  }
+};
+
+const isAddressValid = address => {
+  if (address.length !== 130) {
+    return false;
+  } else if (/^[^a-f0-9]+$/i.test(address)) {
+    return false;
+  } else if (!address.startsWith("04")) {
+    return false;
+  } else {
+    return true;
+  }
+};
+
+const isTxOutStructureValid = txOut => {
+  if (typeof txOut === null) {
+    return false;
+  } else if (typeof txOut.address !== "string") {
+    return false;
+  } else if (!isAddressValid(txOut.address)) {
+    return false;
+  } else if (typeof txOut.amount !== "number") {
+    return false;
+  } else {
+    return true;
+  }
+};
+
+const isTxStructureValid = tx => {
+  if (typeof tx.id !== "string") {
+    console.log("The Tx ID is not valid");
+    return false;
+  } else if (!(tx.txIns instanceof Array)) {
+    console.log("The txIns are not an array");
+    return false;
+  } else if (tx.txIns.some(txIn => !isTxInStructureValid(txIn))) {
+    console.log("The structure of one of the txIn is not valid");
+    return false;
+  } else if (!(tx.txOuts instanceof Array)) {
+    console.log("The txOuts are not an array");
+    return false;
+  } else if (tx.txOuts.some(txOut => !isTxOutStructureValid(txOut))) {
+    console.log("The structure of one of the txOut is not valid");
+    return false;
+  } else {
+    return true;
+  }
 };
