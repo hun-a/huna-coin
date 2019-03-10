bd135fb4453ec082facc09296f09cf150003e068
seunghun <seunghun.chan@gmail.com>
2019-03-10 23:37:24 +0900

    Getting address and balance

--- src/wallet.js
+++ src/wallet.js
@@ -1,6 +1,7 @@
 const fs = require('fs'),
   path = require('path'),
-  EC = require("elliptic").ec;
+  EC = require("elliptic").ec,
+  R = require('ramda');
 
 const ec = new EC("secp256k1");
 
@@ -11,6 +12,22 @@ const generatePrivateKey = () => {
   const privateKey = keyPair.getPrivate();
   return privateKey.toString(16);
 };
+
+const getPrivateFromWallet = () => {
+  const buffer = fs.readFileSync(privateKeyLocation, 'utf-8');
+  return buffer.toString();
+};
+
+const getPublicFromWallet = () => {
+  const privateKey = getPrivateFromWallet();
+  const key = ec.keyFromPrivate(privateKey, 'hex');
+  return key.getPublic().encode('hex');
+};
+
+const getBalance = (address, uTxOutList) => R.pipe(
+  R.filter(uTxO => uTxO.address === address),
+  R.map(R.prop('amount')),
+  R.sum)(uTxOutList);
 
 const initWallet = () => {
   if (fs.existsSync(privateKeyLocation)) {
